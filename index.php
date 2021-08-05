3379d74480dc88ecc8f4cf1bb30a1327fa07b5e2
Bee K <beetimewa@gmail.com>
2021-08-05 11:36:11 -0700

    update

--- index.php
+++ index.php
@@ -45,9 +45,6 @@ function loadAJAX(cat)
 	});
 
 
-
-
-
 }
     
 function toConsole(data)
@@ -60,9 +57,10 @@ function bondJSON(data){
 
 	//identifies the type of data returned
 	$('#filmtitle').html(data.title);
-
-	$.each.(data.films,function(i,item){
+	$('#films').html("");//clears
+	$.each.(data.films,function(i,item){//reloads
 		let myFilm = bondTemplate(item);
+
 		$('<div></div>').html(myFilm).appendTo('#films');
 	});
 
@@ -101,7 +99,21 @@ function bondTemplate(film){
 		<a href="box" class="category">Bond Films By International Box Office Totals</a>
 		<h3 id="filmtitle">Title Will Go Here</h3>
 		<div id="films">
-
+			<!--
+		<div class="film">
+				<b>Film: </b> 1<br />
+				<b>Title: </b> Dr. No<br />
+				<b>Year: </b> 1962<br />
+				<b>Director: </b> Terence Young<br />
+				<b>Producers: </b> Harry Saltzman and Albert R. Broccoli<br />
+				<b>Writers: </b> Richard Maibaum, Johanna Harwood and Berkely Mather<br />
+				<b>Composer: </b> Monty Norman<br />
+				<b>Bond: </b> Sean Connery<br />
+				<b>Budget: </b> $1,000,000.00<br />
+				<b>Box Office: </b> $59,567,035.00<br />
+				<div class="pic"><img src="thumbnails/dr-no.jpg"></div>
+				</div>
+-->
 		</div>
 		<div id="output">Results go here</div>
 	</body>
